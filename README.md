0300a66cd695e4fd6d68e3749b4d09998e1b1fa0
s2a31 <baloghszabolcs43@gmail.com>
2022-05-20 16:20:42 +0300

    Campground Delete
    Added 'Delete' button to the 'show.ejs' page.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # YelpCamp
-YelpCamp is a website where users can create and review campgrounds. This project is part of Colt Steele's Web Dev course on Udemy.
-
-This project using Node.js, Express, MongoDB, and Bootstrap.
+Web Developer Bootcamp YelpCamp project
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -15,7 +14,6 @@ db.once('open', () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -55,12 +53,6 @@ app.put('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`);
-});
-
-app.delete('/campgrounds/:id', async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
